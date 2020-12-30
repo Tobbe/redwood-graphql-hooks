b46a3f0426b0cffb834688b5bbb535d11ff16469
Tobbe Lundberg <tobbe@tlundberg.com>
2020-12-30 14:14:34 +0100

    Use graphql-hooks

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,20 @@
 import { AuthProvider } from '@redwoodjs/auth'
 import netlifyIdentity from 'netlify-identity-widget'
 import ReactDOM from 'react-dom'
-import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
+import {
+  GraphQLClient,
+  ClientContext,
+  useQuery,
+  useMutation,
+} from 'graphql-hooks'
+import { print } from 'graphql'
+import {
+  FatalErrorBoundary,
+  FetchConfigProvider,
+  FlashProvider,
+  GraphQLHooksProvider,
+  useFetchConfig,
+} from '@redwoodjs/web'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 
 import Routes from 'src/Routes'
@@ -11,12 +24,39 @@ import './index.css'
 
 netlifyIdentity.init()
 
+const useQueryAdapter = (query, options) => {
+  return useQuery(print(query), options)
+}
+
+const useMutationAdapter = (query, options) => {
+  return useMutation(print(query), options)
+}
+
+const GraphqlHooksProvider = ({ children }) => {
+  const { uri: url, headers } = useFetchConfig()
+
+  const client = new GraphQLClient({ url, headers })
+
+  return (
+    <ClientContext.Provider value={client}>{children}</ClientContext.Provider>
+  )
+}
+
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
     <AuthProvider client={netlifyIdentity} type="netlify">
-      <RedwoodProvider>
-        <Routes />
-      </RedwoodProvider>
+      <FetchConfigProvider>
+        <GraphqlHooksProvider>
+          <GraphQLHooksProvider
+            useQuery={useQueryAdapter}
+            useMutation={useMutationAdapter}
+          >
+            <FlashProvider>
+              <Routes />
+            </FlashProvider>
+          </GraphQLHooksProvider>
+        </GraphqlHooksProvider>
+      </FetchConfigProvider>
     </AuthProvider>
   </FatalErrorBoundary>,
   document.getElementById('redwood-app')
